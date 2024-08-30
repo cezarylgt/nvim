f6932e7e23898adf72f40fca22f3ba2a8488f04c
nothinh <nothing>
2024-08-30 18:06:04 +0200

    telescope keymaps in keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,9 +5,34 @@ local opts = {
 }
 
 -----------------
+-- Global -------
+-- --------------
+
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>v', ':e $MYVIMRC<CR>', opts)
+
+
+-----------------
 -- Normal mode --
 -----------------
-vim.g.mapleader = " "
+
+require('telescope').setup({})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pws', function()
+    local word = vim.fn.expand("<cword>")
+    builtin.grep_string({ search = word })
+end)
+vim.keymap.set('n', '<leader>pWs', function()
+    local word = vim.fn.expand("<cWORD>")
+    builtin.grep_string({ search = word })
+ end)
+vim.keymap.set('n', '<leader>ps', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
 -- Hint: see `:h vim.map.set()`
 -- Better window navigation
@@ -31,4 +56,3 @@ vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 
-vim.keymap.set('n', '<leader>v', ':e $MYVIMRC<CR>', opts)
